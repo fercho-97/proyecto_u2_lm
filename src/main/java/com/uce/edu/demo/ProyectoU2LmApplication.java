d0d56a6efddc9f138c1748e132a74acabd6b43fe
Fernando Mosquera <luismosquera97@gmail.com>
2022-07-19 20:10:01 -0500

    Mapeo JPA OneToOne

--- src/main/java/com/uce/edu/demo/ProyectoU2LmApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2LmApplication.java
@@ -1,5 +1,6 @@
 package com.uce.edu.demo;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import org.jboss.logging.Logger;
@@ -12,9 +13,12 @@ import com.uce.edu.demo.estudiante.repository.modelo.Estudiante;
 import com.uce.edu.demo.estudiante.repository.modelo.EstudianteContadorCondicion;
 import com.uce.edu.demo.estudiante.repository.modelo.EstudianteSencillo;
 import com.uce.edu.demo.estudiante.service.IEstudianteJpaService;
+import com.uce.edu.demo.repository.modelo.Ciudadano;
+import com.uce.edu.demo.repository.modelo.Empleado;
 import com.uce.edu.demo.repository.modelo.Persona;
 import com.uce.edu.demo.repository.modelo.PersonaContadorGenero;
 import com.uce.edu.demo.repository.modelo.PersonaSencilla;
+import com.uce.edu.demo.service.ICiudadanoService;
 import com.uce.edu.demo.service.IPersonaJpaService;
 
 @SpringBootApplication
@@ -28,6 +32,9 @@ public class ProyectoU2LmApplication implements CommandLineRunner {
 	@Autowired
 	private IEstudianteJpaService iEstudianteJpaService;
 
+	@Autowired
+	private ICiudadanoService iCiudadanoService;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU2LmApplication.class, args);
 	}
@@ -36,21 +43,15 @@ public class ProyectoU2LmApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 
-		List<EstudianteSencillo> listaEstudianteSencillo = this.iEstudianteJpaService
-				.buscarPorApellidoOrNombreSencillo("Buitron", "Pablo");
+		Empleado e1 = new Empleado();
+		e1.setCodigoIess("1845");
+		e1.setSalario(new BigDecimal(100));
 
-		for (EstudianteSencillo item : listaEstudianteSencillo) {
-			LOG.info("Estudiante Sencillo: " + item);
+		Ciudadano c1 = new Ciudadano();
+		c1.setNombre("Nicol");
+		c1.setApellido("Altamirano");
 
-		}
-		
-		List<EstudianteContadorCondicion> listaEstudianteContador = this.iEstudianteJpaService
-				.consultarCantidadSemestre("Promedio");
-
-		for (EstudianteContadorCondicion item : listaEstudianteContador) {
-			LOG.info("EstudianteContadorCondicion: " + item);
-
-		}
+		this.iCiudadanoService.insertar(c1);
 
 	}
 
